e7d16ef1be755068cdefe21ddec96fd1c10e6f65
むずでょ <muzudho1@gmail.com>
2022-05-08 01:37:46 +0900

    Reconnecting

--- host1/webapp1/static/tic-tac-toe2/connection.js
+++ host1/webapp1/static/tic-tac-toe2/connection.js
@@ -23,6 +23,9 @@ class Connection {
         // 1. ホスト アドレス
         // 2. URLの一部
         console.log(`[Debug] Connection#constructor roomName=${this.roomName} myPiece=${this.myPiece} connectionString=${this.connectionString}`)
+
+        // 再接続中表示フラグ
+        this.isReconnectingDisplay = false
     }
 
     /**
@@ -32,32 +35,50 @@ class Connection {
      * @param {*} onCloseWebSocket - Webソケットが閉じられたとき。 例: サーバー側にエラーがあって接続が切れたりなど
      * @param {*} setMessageFromServer - サーバーからのメッセージがセットされる関数
      */
-    setup(onOpenWebSocket, onCloseWebSocket, setMessageFromServer) {
-        console.log(`[Debug] Connection#setup`)
-        this.webSock1 = new WebSocket(this.connectionString);
-        this.webSock1.onopen = onOpenWebSocket;
-        this.webSock1.onclose = onCloseWebSocket;
+    connect(onOpenWebSocket, onCloseWebSocket, setMessageFromServer, onWebSocketError) {
+        console.log(`[Connection#connect] Start`)
 
-        // 設定: サーバーからメッセージを受信したとき
-        this.webSock1.onmessage = (e) => {
-            // JSON を解析、メッセージだけ抽出
-            let data1 = JSON.parse(e.data);
-            let message = data1["message"];
-            setMessageFromServer(message)
-        };
+        // Webソケットを生成すると、接続も行われる。再接続したいときは、再生成する
+        try {
+            // 接続できないと、この生成に失敗する。catch もできない
+            this.webSock1 = new WebSocket(this.connectionString);
 
-        // Webソケットを接続します
-        this.connect();
-    }
+            this.webSock1.onopen = onOpenWebSocket;
+            this.webSock1.onclose = onCloseWebSocket;
 
-    /**
-     * Main function which handles the connection
-     * of websocket.
-     */
-    connect() {
-        if (this.webSock1.readyState == WebSocket.OPEN) {
-            console.log('Open socket.');
-            this.webSock1.onopen();
+            // 設定: サーバーからメッセージを受信したとき
+            this.webSock1.onmessage = (e) => {
+                // JSON を解析、メッセージだけ抽出
+                let data1 = JSON.parse(e.data);
+                let message = data1["message"];
+                setMessageFromServer(message)
+            };
+
+            // this.webSock1.onerror = onWebSocketError;
+            this.webSock1.addEventListener('error', (event1) => {
+                onWebSocketError(event1)
+            })
+
+            // 状態を表示
+            if (this.webSock1.readyState == WebSocket.CONNECTING) {
+                // 未接続
+                console.log('[Connect] Connecting socket.');
+            } else if (this.webSock1.readyState == WebSocket.OPEN) {
+                console.log('[Connect] Open socket.');
+                this.webSock1.onopen();
+            } else if (this.webSock1.readyState == WebSocket.CLOSING) {
+                console.log('[Connect] Closing socket.');
+            } else if (this.webSock1.readyState == WebSocket.CLOSED) {
+                // サーバーが落ちたりしたときは、ここ
+                console.log('[Connect] Closed socket.');
+            } else {
+                console.log(`[Connect] webSock1.readyState=${this.webSock1.readyState}`);
+            }
+
+        } catch (error) {
+            // キャッチで捕まえられない
+            console.log(`[Connect] Exception ${error}`);
         }
+
     }
 }
--- host1/webapp1/static/tic-tac-toe2/engine.js
+++ host1/webapp1/static/tic-tac-toe2/engine.js
@@ -8,31 +8,15 @@ class Engine {
      * @param {*} reconnect - 再接続ラムダ関数
      */
     constructor(onSetMessageFromServer, reconnect) {
+        this._onSetMessageFromServer = onSetMessageFromServer
+        this._reconnect = reconnect
+
         // 接続
         this._connection = new Connection();
         // メッセージ一覧
         this._protocolMessages = new ProtocolMessages();
         // ゲーム
         this._game = new Game();
-
-        this._connection.setup(
-            // Webソケットを開かれたとき
-            () => {
-                console.log('WebSockets connection created.');
-                let response = this.protocolMessages.createStart()
-                this._connection.webSock1.send(JSON.stringify(response))
-            },
-            // Webソケットが閉じられたとき
-            (e) => {
-                console.log(`Socket is closed. Reconnect will be attempted in 1 second. ${e.reason}`);
-                setTimeout(() => {
-                    // このコードブロックでは、 this の指しているものが Engine ではないので注意
-                    reconnect();
-                }, 1000);
-            },
-            // サーバーからのメッセージを受信したとき
-            onSetMessageFromServer,
-        )
 
         // １手進めたとき
         this._game.onDoMove = (sq, myPiece) => {
@@ -51,6 +35,8 @@ class Engine {
             let response = this.protocolMessages.createDraw()
             this._connection.webSock1.send(JSON.stringify(response))
         }
+
+        this.connect()
     }
 
     /**
@@ -72,5 +58,31 @@ class Engine {
      */
     get game() {
         return this._game
+    }
+
+    /**
+     * 接続
+     */
+    connect() {
+        this._connection.connect(
+            // Webソケットを開かれたとき
+            () => {
+                console.log('WebSockets connection created.');
+                let response = this.protocolMessages.createStart()
+                this._connection.webSock1.send(JSON.stringify(response))
+            },
+            // Webソケットが閉じられたとき
+            (e) => {
+                console.log(`Socket is closed. Reconnect will be attempted in 1 second. ${e.reason}`);
+                // 1回だけ再接続を試みます
+                this._reconnect()
+            },
+            // サーバーからのメッセージを受信したとき
+            this._onSetMessageFromServer,
+            // エラー時
+            (e) => {
+                console.log(`Socket is error. ${e.reason}`);
+            }
+        )
     }
 }
