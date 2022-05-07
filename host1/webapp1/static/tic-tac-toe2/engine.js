d813f1feb6e6b4315479ef46807b5a23f1064c22
むずでょ <muzudho1@gmail.com>
2022-05-08 00:26:34 +0900

    Reconnect

--- host1/webapp1/static/tic-tac-toe2/engine.js
+++ host1/webapp1/static/tic-tac-toe2/engine.js
@@ -5,8 +5,9 @@ class Engine {
     /**
      * 生成
      * @param {*} onSetMessageFromServer - サーバーからのメッセージをセットする関数
+     * @param {*} reconnect - 再接続ラムダ関数
      */
-    constructor(onSetMessageFromServer) {
+    constructor(onSetMessageFromServer, reconnect) {
         // 接続
         this._connection = new Connection();
         // メッセージ一覧
@@ -19,13 +20,14 @@ class Engine {
             () => {
                 console.log('WebSockets connection created.');
                 let response = this.protocolMessages.createStart()
-                this.connection.webSock1.send(JSON.stringify(response))
+                this._connection.webSock1.send(JSON.stringify(response))
             },
             // Webソケットが閉じられたとき
             (e) => {
                 console.log(`Socket is closed. Reconnect will be attempted in 1 second. ${e.reason}`);
-                setTimeout(function () {
-                    this.connection.connect();
+                setTimeout(() => {
+                    // このコードブロックでは、 this の指しているものが Engine ではないので注意
+                    reconnect();
                 }, 1000);
             },
             // サーバーからのメッセージを受信したとき
@@ -35,19 +37,19 @@ class Engine {
         // １手進めたとき
         this._game.onDoMove = (sq, myPiece) => {
             let response = this.protocolMessages.createDoMove(sq, myPiece)
-            this.connection.webSock1.send(JSON.stringify(response))
+            this._connection.webSock1.send(JSON.stringify(response))
         }
 
         // どちらかが勝ったとき
         this._game.onWon = (myPiece) => {
             let response = this.protocolMessages.createWon(myPiece)
-            this.connection.webSock1.send(JSON.stringify(response))
+            this._connection.webSock1.send(JSON.stringify(response))
         }
 
         // 引き分けたとき
         this._game.onDraw = () => {
             let response = this.protocolMessages.createDraw()
-            this.connection.webSock1.send(JSON.stringify(response))
+            this._connection.webSock1.send(JSON.stringify(response))
         }
     }
 
