69d2d60d9814f48c6a86b1c77821f517b682b669
むずでょ <muzudho1@gmail.com>
2022-05-29 17:22:37 +0900

    staticmethodへ変更

--- host1/webapp1/models_helper/mh_member.py
+++ host1/webapp1/models_helper/mh_member.py
@@ -1,4 +1,5 @@
 import json
+from os import stat
 from django.core import serializers
 
 
@@ -11,33 +12,37 @@ from webapp1.models.m_member import Member
 # 4. クラス名
 
 
-def get_all_members():
+class MhMember():
 
-    # id順に要素を全部取得
-    dbMemberQuerySet = Member.objects.all().order_by('id')
-    # memberSet=<QuerySet [<Member: きふわらね>, <Member: きふわらずさ>, <Member: きふわらかく>, <Member: ほげQ>, <Member: ひょろ>]>
-    print(f"dbMemberQuerySet={dbMemberQuerySet}")
+    @staticmethod
+    def get_all_members():
+        """TODO 使ってない？"""
 
-    # JSON 文字列に変換
-    dbMemberJsonStr = serializers.serialize('json', dbMemberQuerySet)
+        # id順に要素を全部取得
+        dbMemberQuerySet = Member.objects.all().order_by('id')
+        # memberSet=<QuerySet [<Member: きふわらね>, <Member: きふわらずさ>, <Member: きふわらかく>, <Member: ほげQ>, <Member: ひょろ>]>
+        print(f"dbMemberQuerySet={dbMemberQuerySet}")
 
-    # オブジェクトに変換
-    dbMemberDoc = json.loads(dbMemberJsonStr)
+        # JSON 文字列に変換
+        dbMemberJsonStr = serializers.serialize('json', dbMemberQuerySet)
 
-    # 使いやすい形に変換します
-    # 人がいっぱいいるからパーク
-    parkDic = dict()
-    for dbMember in dbMemberDoc:
-        # Example:
-        # dbMember= --> {'model': 'webapp1.member', 'pk': 1, 'fields': {'name': 'きふわらね', 'email': 'kifuwarane@example.com', 'age': 8, 'stateInPark': 0}} <--
-        print(f"dbMember= --> {dbMember} <--")
+        # オブジェクトに変換
+        dbMemberDoc = json.loads(dbMemberJsonStr)
 
-        parkDic[dbMember["pk"]] = {
-            "pk": dbMember["pk"],
-            "name": dbMember["fields"]["name"],
-            "board": dbMember["fields"]["email"],
-            "record": dbMember["fields"]["age"],
-            "record": dbMember["fields"]["stateInPark"],
-        }
+        # 使いやすい形に変換します
+        # 人がいっぱいいるからパーク
+        parkDic = dict()
+        for dbMember in dbMemberDoc:
+            # Example:
+            # dbMember= --> {'model': 'webapp1.member', 'pk': 1, 'fields': {'name': 'きふわらね', 'email': 'kifuwarane@example.com', 'age': 8, 'stateInPark': 0}} <--
+            print(f"dbMember= --> {dbMember} <--")
 
-    return parkDic
+            parkDic[dbMember["pk"]] = {
+                "pk": dbMember["pk"],
+                "name": dbMember["fields"]["name"],
+                "board": dbMember["fields"]["email"],
+                "record": dbMember["fields"]["age"],
+                "record": dbMember["fields"]["stateInPark"],
+            }
+
+        return parkDic
