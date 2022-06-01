1c2b64a90b8dacbfe6f3706b1d1d0a8c3ddfce44
むずでょ <muzudho1@gmail.com>
2022-06-01 23:41:14 +0900

    コメント追加

--- host1/webapp1/views/v_practice_of_page1.py
+++ host1/webapp1/views/v_practice_of_page1.py
@@ -4,5 +4,10 @@ from django.template import loader
 
 def render_page1(request):
     template = loader.get_template('webapp1/practice/page1.html')
+    #                               ---------------------------
+    #                               1
+    # 1. host1/webapp1/templates/webapp1/practice/page1.html を取得
+    #                            ---------------------------
+
     context = {}
     return HttpResponse(template.render(context, request))
