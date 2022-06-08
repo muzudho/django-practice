00e32da3ac6b6e7448f0caff1e7c1ddd7fccbc8e
むずでょ <muzudho1@gmail.com>
2022-06-09 00:55:02 +0900

    関数化

--- host1/webapp1/views/v_tic_tac_toe_v1.py
+++ host1/webapp1/views/v_tic_tac_toe_v1.py
@@ -5,44 +5,59 @@ from django.shortcuts import render, redirect
 class MatchApplication():
     """対局申込"""
 
-    @staticmethod
+    _path_of_playing = "/tic-tac-toe/v1/playing/{0}/?&mypiece={1}"
+    #                   -----------------------------------------
+    #                   1
+    # 1. http://example.com:8000/tic-tac-toe/v1/playing/Elephant/?&mypiece=X
+    #                           --------------------------------------------
+
+    _path_of_html = "webapp1/tic-tac-toe/v1/match_application.html"
+    #                ---------------------------------------------
+    #                1
+    # 1. host1/webapp1/templates/webapp1/tic-tac-toe/v1/match_application.html
+    #                            ---------------------------------------------
+
     def render(request):
         """描画"""
-        if request.method == "POST":
-            # 送信後
-
-            room_name = request.POST.get("room_name")
-            myPiece = request.POST.get("my_piece")
-            return redirect(f'/tic-tac-toe/v1/playing/{room_name}/?&mypiece={myPiece}')
-            #                 -------------------------------------------------------
-            #                 1
-            # 1. http://example.com:8000/tic-tac-toe/v1/playing/Elephant/?&mypiece=X
-            #                           --------------------------------------------
-
-        # 訪問後
-        return render(request, "webapp1/tic-tac-toe/v1/match_application.html", {})
-        #                       ---------------------------------------------
-        #                       1
-        # 1. host1/webapp1/templates/webapp1/tic-tac-toe/v1/match_application.html
-        #                            ---------------------------------------------
+        return match_application_render(request, MatchApplication._path_of_playing, MatchApplication._path_of_html)
 
 
 class Playing():
     """対局画面"""
 
-    @staticmethod
+    _path_of_html = "webapp1/tic-tac-toe/v1/playing.html"
+    #                -----------------------------------
+    #                1
+    # 1. host1/webapp1/templates/webapp1/tic-tac-toe/v1/playing.html
+    #                            -----------------------------------
+
     def render(request, room_name):
         """描画"""
+        return playing_render(request, room_name, Playing._path_of_html)
 
-        myPiece = request.GET.get("mypiece")
-        if myPiece not in ['X', 'O']:
-            raise Http404(f"My piece '{myPiece}' does not exists")
-        context = {
-            "my_piece": myPiece,
-            "room_name": room_name
-        }
-        return render(request, "webapp1/tic-tac-toe/v1/playing.html", context)
-        #                       -----------------------------------
-        #                       1
-        # 1. host1/webapp1/templates/webapp1/tic-tac-toe/v1/playing.html
-        #                            -----------------------------------
+
+# 以下、関数
+
+
+def match_application_render(request, path_of_playing, path_of_html):
+    """対局申込 - 描画"""
+    if request.method == "POST":
+        # 送信後
+        room_name = request.POST.get("room_name")
+        myPiece = request.POST.get("my_piece")
+        # TODO バリデーションチェックしたい
+        return redirect(path_of_playing.format(room_name, myPiece))
+
+    # 訪問後
+    return render(request, path_of_html, {})
+
+
+def playing_render(request, room_name, path_of_html):
+    myPiece = request.GET.get("mypiece")
+    if myPiece not in ['X', 'O']:
+        raise Http404(f"My piece '{myPiece}' does not exists")
+    context = {
+        "my_piece": myPiece,
+        "room_name": room_name
+    }
+    return render(request, path_of_html, context)
