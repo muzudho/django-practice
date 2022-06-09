1dcbd4066fe1aa377af268dcdec64efb51f80f89
むずでょ <muzudho1@gmail.com>
2022-06-09 22:37:58 +0900

    WIP

--- host1/webapp1/views/v_tic_tac_toe_v3o1.py
+++ host1/webapp1/views/v_tic_tac_toe_v3o1.py
@@ -1,23 +1,21 @@
-from webapp1.views.v_tic_tac_toe_v3 import MatchApplication as MatchApplicationV3
-#    ------- ----- ----------------        ----------------    ------------------
-#    1       2     3                       4                   5
+from webapp1.views import v_tic_tac_toe_v2
+#                                        ^ two
+#    ------- -----        ----------------
+#    1       2            3
 # 1. アプリケーション フォルダー名
 # 2. ディレクトリー名
 # 3. Python ファイル名。拡張子抜き
-# 4. クラス名
-# 5. `4.` に付けた別名
 
-from webapp1.views.v_tic_tac_toe_v3 import Playing as PlayingV3
-#    ------- ----- ----------------        -------    ---------
-#    1       2     3                       4          5
+from webapp1.views import v_tic_tac_toe_v3
+#                                        ^ three
+#    ------- -----        ----------------
+#    1       2            3
 # 1. アプリケーション フォルダー名
 # 2. ディレクトリー名
 # 3. Python ファイル名。拡張子抜き
-# 4. クラス名
-# 5. `4.` に付けた別名
 
 
-class MatchApplication(MatchApplicationV3):
+class MatchApplication():
     """対局申込ページ"""
 
     _path_of_playing = "/tic-tac-toe/v3o1/playing/{0}/?&mypiece={1}"
@@ -34,8 +32,25 @@ class MatchApplication(MatchApplicationV3):
     # 1. host1/webapp1/templates/webapp1/tic-tac-toe/v2/match_application.html
     #                            ---------------------------------------------
 
+    @staticmethod
+    def render(request):
+        """描画"""
+        return v_tic_tac_toe_v2.match_application_render(request, MatchApplication._path_of_playing, MatchApplication._path_of_html, MatchApplication.on_sent, MatchApplication.on_visited)
+        #                     ^ two
 
-class Playing(PlayingV3):
+    @staticmethod
+    def on_sent(request):
+        """送信後"""
+        return v_tic_tac_toe_v3.match_application_on_sent(request)
+
+    @staticmethod
+    def on_visited(request):
+        """訪問後"""
+        # 拡張したい挙動があれば、ここに書く
+        pass
+
+
+class Playing():
 
     _path_of_playing = "/tic-tac-toe/v3o1/playing/"
     #                                 ^^^ three o one
@@ -43,3 +58,22 @@ class Playing(PlayingV3):
     #                   1
     # 1. http://example.com/tic-tac-toe/v3o1/playing/Elephant/
     #                      --------------------------
+
+    _path_of_html = "webapp1/tic-tac-toe/v3/playing.html.txt"
+    #                                     ^ three
+    #                ---------------------------------------
+    #                1
+    # 1. host1/webapp1/templates/webapp1/tic-tac-toe/v3/playing.html.txt
+    #                            ---------------------------------------
+
+    @staticmethod
+    def render(request, kw_room_name):
+        """描画"""
+        return v_tic_tac_toe_v2.playing_render(request, kw_room_name, Playing._path_of_playing, Playing._path_of_html, Playing.on_update)
+        #                     ^ two
+
+    @staticmethod
+    def on_update(request):
+        """訪問後または送信後"""
+        # 拡張したい挙動があれば、ここに書く
+        pass
