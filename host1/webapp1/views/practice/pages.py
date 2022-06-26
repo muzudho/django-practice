03efc7652f97337726b91284d648cee187c378e5
むずでょ <muzudho1@gmail.com>
2022-06-26 18:09:47 +0900

    django-practice2 へ移動

--- host1/webapp1/views/practice/pages.py
+++ host1/webapp1/views/practice/pages.py
@@ -13,41 +13,6 @@ from webapp1.models_helper.mh_user import MhUser
 # 4. クラス名
 
 
-def render_page1(request):
-    template = loader.get_template('webapp1/practice/page1.html')
-    #                               ---------------------------
-    #                               1
-    # 1. host1/webapp1/templates/webapp1/practice/page1.html を取得
-    #                            ---------------------------
-
-    context = {}
-    return HttpResponse(template.render(context, request))
-
-
-def render_page2_patch1(request):
-    """ページ２　パッチ１"""
-    template = loader.get_template('webapp1/practice/page2_patch1.html.txt')
-    #                               --------------------------------------
-    #                               1
-    # 1. host1/webapp1/templates/webapp1/practice/page2_patch1.html.txt を取得
-    #                            --------------------------------------
-
-    context = {}
-    return HttpResponse(template.render(context, request))
-
-
-def render_page2_patch2(request):
-    """ページ２　パッチ２"""
-    template = loader.get_template('webapp1/practice/page2_patch2.html.txt')
-    #                               --------------------------------------
-    #                               1
-    # 1. host1/webapp1/templates/webapp1/practice/page2_patch2.html.txt を取得
-    #                            --------------------------------------
-
-    context = {}
-    return HttpResponse(template.render(context, request))
-
-
 def render_user_list(request):
     """会員登録ユーザー一覧"""
 
